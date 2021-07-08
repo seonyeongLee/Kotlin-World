2f13cb0c63c0fba42dc23c31a6930930b961d6ae
seonyeongLee <qrsty9636@gmail.com>
2021-07-08 22:07:50 +0900

    change input to bootstrap

--- mw-web/src/components/input.js
+++ mw-web/src/components/input.js
@@ -1,12 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, InputGroup, FormControl} from "react-bootstrap";
+
 function Input(props) {
     return (
-        <form onSubmit={props.handleSubmit}>
-            <label>
-            Sample &nbsp;
-            <input type="text" required={true} value={props.input} onChange={props.handleChange}/>
-            </label>
-            <input type="submit" value="Create"/> 
-        </form>
+        // <form onSubmit={props.handleSubmit}>
+        //     <label>
+        //     Sample &nbsp;
+        //     <input type="text" required={true} value={props.input} onChange={props.handleChange}/>
+        //     </label>
+        //     <input type="submit" value="Create"/> 
+        // </form>
+
+        <InputGroup className="mb-3">
+            <FormControl 
+                aria-label="Sample"
+                placeholder="입력해주세요."
+                aria-describedby="basic-addon2"
+                required={true} value={props.input} onChange={props.handleChange}
+            />
+            <Button onClick={props.handleSubmit} variant="outline-secondary" id="button-addon2">
+            Create
+            </Button>
+        </InputGroup>
     )
 }
 
